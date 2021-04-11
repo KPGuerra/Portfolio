39189e7917d56634679644d033eb15b2ccba374b
KPGuerra <KPGuerra96@gmail.com>
2021-04-10 20:28:33 -0400

    Updating Skills

---
+++ portfolio/src/Components/Content/Skills/SkillCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function SkillCards (props) {
+    let languages = ["Ruby", "JavaScript","React.js", "Redux","Rails", "HTML", "CSS"]
+    let patterns = ["ActiveRecord", "MVC", "ActiveStorage", "RESTful API", "JWT", "OAuth"]
+    return (
+        <h3> Programming Languages </h3>
+        
+    )
+}
