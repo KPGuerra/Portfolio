dd3eeb2d50475d2030b377162304501ff83396b3
KPGuerra <KPGuerra96@gmail.com>
2021-04-17 18:25:50 -0400

    Contact Page Content Done

---
+++ portfolio/src/Components/Content/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function ContactPage() {
+    return (
+        <>
+            <h1> Contact & Socials </h1>
+            <h2> Where you can find me on the internet :) </h2>
+
+            <div>
+                <h3> LinkedIn </h3>
+                <img src="" alt="LinkedIn Logo" width="200" height="200"/>
+                <a href="https://www.linkedin.com/in/karinapguerra"> LinkedIn Profile </a>
+            </div>
+
+            <div>
+                <h3> Github </h3>
+                <img src="" alt="Github Logo" width="200" height="200"/>
+                <a href="https://github.com/KPGuerra"> Github Profile </a>
+
+            </div>
+
+            <div>
+                <h3> Email </h3>
+                <img src="" alt="Gmail Logo" width="200" height="200"/>
+                <a href=""> KMPGuerra@gmail.com</a>
+            </div>
+
+
+        </>
+    )
+}
+
+export default ContactPage
