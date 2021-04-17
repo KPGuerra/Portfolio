acacc73126aeccb2654a2223f5c833668e2e91b7
KPGuerra <KPGuerra96@gmail.com>
2021-04-17 18:39:41 -0400

    Contact Content Updated

--- portfolio/src/Components/Content/Contact/Contact.jsx
+++ portfolio/src/Components/Content/Contact/Contact.jsx
@@ -8,20 +8,25 @@ function ContactPage() {
 
             <div>
                 <h3> LinkedIn </h3>
-                <img src="" alt="LinkedIn Logo" width="200" height="200"/>
+                <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpragmaticcoders.com%2Fwp-content%2Fuploads%2F2014%2F12%2Flinkedin-logo.png&f=1&nofb=1" alt="LinkedIn Logo" width="150" height="140"/>
                 <a href="https://www.linkedin.com/in/karinapguerra"> LinkedIn Profile </a>
             </div>
 
             <div>
                 <h3> Github </h3>
-                <img src="" alt="Github Logo" width="200" height="200"/>
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F09%2FGitHub_logo.png&f=1&nofb=1" alt="Github Logo" width="150" height="140"/>
                 <a href="https://github.com/KPGuerra"> Github Profile </a>
+            </div>
 
+            <div>
+                <h3> Medium </h3>
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.underconsideration.com%2Fbrandnew%2Farchives%2Fmedium_2017_monogram.png&f=1&nofb=1" alt="Medium Logo" width="150" height="140"/>
+                <a href="https://kpguerra96.medium.com/"> My Medium Profile </a>
             </div>
 
             <div>
                 <h3> Email </h3>
-                <img src="" alt="Gmail Logo" width="200" height="200"/>
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freelogovectors.net%2Fwp-content%2Fuploads%2F2020%2F10%2Fgmail_logo_icon.png&f=1&nofb=1" alt="Gmail Logo" width="150" height="140"/>
                 <a href=""> KMPGuerra@gmail.com</a>
             </div>
 
