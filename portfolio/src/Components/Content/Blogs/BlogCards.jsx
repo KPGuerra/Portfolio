0c463c112566c4b9807a2d0bd78288e4af55ac84
KPGuerra <KPGuerra96@gmail.com>
2021-05-08 17:42:30 -0400

    Formatted Blog Date

--- portfolio/src/Components/Content/Blogs/BlogCards.jsx
+++ portfolio/src/Components/Content/Blogs/BlogCards.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 
-function BlogCards (props) {
+function BlogCards(props) {
     const blogObj = props.blogObject
+
+    const dateHandler = (date) => {
+        let dateString = date.split(" ")
+        let dateArr = dateString[0]
+        let arr = dateArr.split("-")
+
+        let month = arr[1]
+        let day = arr[2]
+        let year = arr[0]
+
+        return `${month}/${day}/${year}`
+
+    }
 
     return (
         <div>
             <h1>{blogObj.title}</h1>
-            <img src={blogObj.thumbnail} width="470" height="250"/>
-            <p> {blogObj.pubDate}</p>
-            
+            <img src={blogObj.thumbnail} width="470" height="250" />
+            <p> {dateHandler(blogObj.pubDate)}</p>
+
         </div>
     )
 }
